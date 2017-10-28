3c4398bf007cbf762a17bb4beb9fe82738474096
andkcho <{andkcho}@users.noreply.github.com>
2017-10-28 11:27:38 -0600

    up HW

--- javascript.js
+++ javascript.js
@@ -21,7 +21,7 @@ function populateButtons (searchArray,classToAdd,areaToAddTo) {
 $(document).on("click",".searchButton", function() {
 	var type = $(this).data("type");
 	//console.log(type); 
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +type+ "&api_key=dc6zaTOxFJmzC&limit=20";
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +type+ "&api_key=dc6zaTOxFJmzC&limit=20";
 	$.ajax({url:queryURL,method:"GET"})
 	.done(function(response) {
 		for (i = 0; i<response.data.length; i++){
